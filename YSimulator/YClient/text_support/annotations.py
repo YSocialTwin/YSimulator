9cdcb5b191e20645b7c9ab5b0f53a91df8ab699a
GiulioRossetti <giulio.rossetti@gmail.com>
2025-12-23 08:56:09 +0100

    :arrow_up: post/comment handling and annotation

---
+++ YSimulator/YClient/text_support/annotations.py
@@ -0,0 +1,57 @@
+from nltk.sentiment import SentimentIntensityAnalyzer
+from perspective import PerspectiveAPI
+
+
+def vader_sentiment(text: str) -> dict:
+    """
+    Calculate sentiment scores using VADER.
+
+    :param text: the text to analyze
+    :return: a dictionary with sentiment scores
+    """
+    sia = SentimentIntensityAnalyzer()
+    sentiment = sia.polarity_scores(text)
+    return sentiment
+
+
+def toxicity(text, api_key: str) -> dict:
+    """
+    Calculate toxicity scores using the Perspective API.
+
+    :param text: the text to analyze
+    :param api_key: the Perspective API key
+    :return: a dictionary with toxicity scores
+    """
+
+    if api_key is not None:
+        try:
+            p = PerspectiveAPI(api_key)
+            toxicity_score = p.score(
+                text,
+                tests=[
+                    "TOXICITY",
+                    "SEVERE_TOXICITY",
+                    "IDENTITY_ATTACK",
+                    "INSULT",
+                    "PROFANITY",
+                    "THREAT",
+                    "SEXUALLY_EXPLICIT",
+                    "FLIRTATION",
+                ],
+            )
+
+            return {
+                "TOXICITY": toxicity_score["TOXICITY"],
+                "SEVERE_TOXICITY": toxicity_score["SEVERE_TOXICITY"],
+                "IDENTITY_ATTACK": toxicity_score["IDENTITY_ATTACK"],
+                "INSULT": toxicity_score["INSULT"],
+                "PROFANITY": toxicity_score["PROFANITY"],
+                "THREAT": toxicity_score["THREAT"],
+                "SEXUALLY_EXPLICIT": toxicity_score["SEXUALLY_EXPLICIT"],
+                "FLIRTATION": toxicity_score["FLIRTATION"],
+            }
+
+        except Exception as e:
+            return {}
+    return {}
+
---
+++ YSimulator/YClient/text_support/cleaning.py
@@ -0,0 +1,46 @@
+import re
+
+
+def clean_text(author: str, text: str) -> str:
+    """
+    Clean the text by removing unwanted characters and the author's self mention.
+
+    :param author: the author's username
+    :param text: the text to clean
+    :return: the cleaned text
+    """
+    text = (
+        text
+        .replace("-", "")
+        .replace("@ ", "")
+        .replace("  ", " ")
+        .replace(". ", ".")
+        .replace(" ,", ",")
+        .replace("[", "")
+        .replace("]", "")
+        .replace("@,", "")
+        .strip("()[]{}'")
+        .lstrip()
+    )
+    text = text.replace(f"@{author}", "")
+    return text
+
+
+def extract_components(text: str, c_type: str = "hashtags") -> list:
+    """
+    Extract the components from the text.
+
+    :param text: the text to extract the components from
+    :param c_type: the component type, either "hashtags" or "mentions"
+    :return: the extracted components
+    """
+    # Define the regex pattern
+    if c_type == "hashtags":
+        pattern = re.compile(r"#\w+")
+    elif c_type == "mentions":
+        pattern = re.compile(r"@\w+")
+    else:
+        return []
+    # Find all matches in the input text
+    components = pattern.findall(text)
+    return components
